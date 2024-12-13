b941117a2a881be75e06175a3f4f371a52d163e6
Xavier Marquez <marquez.x@gmail.com>
2024-12-13 19:36:46 +1300

    Lots of changes to pipeline for more flexiblity

--- R/model_utilities.R
+++ R/model_utilities.R
@@ -1,13 +1,16 @@
-load_model_costs <- function() {
-  json_file <- "https://raw.githubusercontent.com/BerriAI/litellm/refs/heads/main/model_prices_and_context_window.json"
-  res <- jsonlite::fromJSON(json_file) |>
+load_model_costs <- function(url) {
+  res <- jsonlite::fromJSON(url) |>
     purrr::map(\(x) tibble::as_tibble_row(x) |>
                  dplyr::mutate(dplyr::across(dplyr::everything(), as.character))) |>
     purrr::list_rbind(names_to = "model") |>
     dplyr::filter(model != "sample_spec") |>
     dplyr::mutate(dplyr::across(dplyr::matches("max_|cost_|output_vector_size"), as.numeric),
                   dplyr::across(dplyr::matches("supports"), as.logical))
-  res
+  res |>
+    select(model, source, litellm_provider, input_cost_per_token, output_cost_per_token) |>
+    mutate(model = str_remove(model, ".+/")) |>
+    filter(litellm_provider %in% c("anthropic", "gemini", "groq", "openai", "mistral")) |>
+    distinct()
 
 }
 
@@ -16,27 +19,50 @@ models_df <- bind_rows(
     api = "groq",
     model = c("llama3-8b-8192",
               "llama3-70b-8192",
-              "llama-3.1-8b-instant",
-              "mixtral-8x7b-32768",
-              "gemma2-9b-it")),
+              "llama-3.1-8b-instant"),
+    type = "single",
+    fun = "call_api"),
+  expand_grid(
+    api = "llamafile",
+    model = c("Llama-3.2-1B-Instruct.Q6_K",
+              "Llama-3.2-3B-Instruct.Q6_K",
+              "gemma-2-2b-it.Q6_K"),
+    type = "single",
+    fun = "call_api"),
+  expand_grid(
+    api = "mistral",
+    model = c(
+      "mistral-small-latest",
+      "ministral-8b-latest",
+      "mistral-medium-latest",
+      "open-mixtral-8x7b"
+    ),
+    type = "batch",
+    fun = "call_batch_api"),
   expand_grid(
     api = "claude",
     model = c(
       # "claude-3-5-sonnet-20241022",
       "claude-3-haiku-20240307"
-    )),
+    ),
+    type = "batch",
+    fun = "call_batch_api"),
   expand_grid(
     api = "openai",
     model = c(
-      # "gpt-4o",
+      "gpt-4o",
       "gpt-4o-mini"
-    )),
+    ),
+    type = "batch",
+    fun = "call_batch_api"),
   expand_grid(
     api = "gemini",
     model = c(
       # "gemini-1.5-pro-latest",
       "gemini-1.5-flash-latest"
-    ))
+    ),
+    type = "single",
+    fun = "call_api")
 )
 
 prompt_names <- c("democracy_simple")
@@ -46,8 +72,10 @@ dataset_names <- c("vdem")
 api_names <- c(
   "claude",
   "openai",
+  "mistral",
   "groq",
-  "gemini"
+  "gemini",
+  "llamafile"
   )
 
 prompt_files_df <- expand_grid(
@@ -56,8 +84,12 @@ prompt_files_df <- expand_grid(
     dataset_name = dataset_names,
     prompt_type = c("user")) |>
   mutate(prompt_filename = paste(api, prompt_name, prompt_type, "prompt.md", sep = "_"),
-         prompt = paste("prompt", api, prompt_name, sep = "_") |>
+         prompt = paste("prompt", api, prompt_name, dataset_name, sep = "_") |>
            rlang::syms(),
+         dataset_name = case_when(api == "groq" & dataset_name == "vdem" ~ "vdem_sample",
+                                  api == "claude" & dataset_name == "vdem" ~ "vdem_grouped",
+                                  api == "gemini" & dataset_name == "vdem" ~ "vdem_grouped_country",
+                                  TRUE ~ dataset_name),
          dataset = dataset_name |>
            rlang::syms()) |>
   group_by(prompt, api, prompt_name, dataset_name, dataset) |>
@@ -65,41 +97,77 @@ prompt_files_df <- expand_grid(
 
 responses_df <- prompt_files_df |>
   inner_join(models_df, relationship = "many-to-many") |>
-  mutate(responses = paste("responses", prompt_name, model, sep = "-") |>
+  mutate(responses = paste("responses", prompt_name, dataset_name, model, sep = "-") |>
            str_replace_all("\\-", "_") |>
            rlang::syms(),
-         responses_joined = paste("responses_joined", prompt_name, model, sep = "-") |>
+         responses_joined = paste("responses_joined", prompt_name, dataset_name, model, sep = "-") |>
            str_replace_all("\\-", "_") |>
-           rlang::syms())
+           rlang::syms(),
+         batch_download_fun = case_when(type == "batch" ~ paste(api, "poll_and_retrieve", sep = "_")))
 
-claude_democracy_simple_response_validation <- function(response) {
-  R.AI:::claude_default_response_validation(response)
+extract_and_join <- function(response, dataset) {
+  response <- poll_and_download(response, quiet = TRUE)
+
+  dataset$id <- 1:nrow(dataset) |>
+    as.character()
+
+  dataset <- dataset |>
+    left_join(response, by = join_by(id)) |>
+    mutate(score = stringr::str_extract(
+      response,
+      "(?<=.?\\<.?democracy.?\\>)(0(?:\\.\\d+)?|1(?:\\.0+)?)(?=.?\\<)"
+    ) |>
+      as.numeric())
+
+  dataset
+
 }
 
-claude_democracy_simple_content_extraction <- function(response) {
-  R.AI:::claude_default_content_extraction(response)
+extract_and_join_single <- function(response, dataset) {
+  dataset$id <- 1:nrow(dataset) |>
+    as.character()
+
+  dataset <- dataset |>
+    left_join(response, by = join_by(id)) |>
+    unnest(response) |>
+    mutate(score = stringr::str_extract(
+      response,
+      "(?<=.?\\<.?democracy.?\\>)(0(?:\\.\\d+)?|1(?:\\.0+)?)(?=.?\\<)"
+    ) |>
+      as.numeric())
+
+  dataset
+
 }
 
-openai_democracy_simple_response_validation <- function(response) {
-  R.AI:::openai_default_response_validation(response)
-}
-
-openai_democracy_simple_content_extraction <- function(response) {
-  R.AI:::openai_default_content_extraction(response)
-}
-
-gemini_democracy_simple_response_validation <- function(response) {
-  R.AI:::gemini_default_response_validation(response)
-}
-
-gemini_democracy_simple_content_extraction <- function(response) {
-  R.AI:::gemini_default_content_extraction(response)
-}
-
-groq_democracy_simple_response_validation <- function(response) {
-  R.AI:::groq_default_response_validation(response)
-}
-
-groq_democracy_simple_content_extraction <- function(response) {
-  R.AI:::groq_default_content_extraction(response)
+estimate_costs <- function(df, model_costs) {
+  res <- df |>
+    left_join(model_costs) |>
+    select(where(\(x) !all(is.na(x)))) |>
+    group_by(model) |>
+    mutate(across(ends_with("_cost_per_token"), \(x) case_when(api_type == "batch" ~ x/2,
+                                                               TRUE ~ x)),
+           input_tokens = case_when(api_name %in% c("openai", "mistral") ~ prompt_tokens,
+                                    TRUE ~ input_tokens),
+           output_tokens = case_when(api_name %in% c("openai", "mistral") ~ completion_tokens,
+                                    TRUE ~ output_tokens),
+           input_cost_per_token = case_when(model == "mistral-small-latest" ~ 0.2/2000000,
+                                            TRUE ~ input_cost_per_token),
+           output_cost_per_token = case_when(model == "mistral-small-latest" ~ 0.6/2000000,
+                                             TRUE ~ output_cost_per_token),
+           input_cost_per_token = case_when(model == "ministral-8b-latest" ~ 0.1/2000000,
+                                            TRUE ~ input_cost_per_token),
+           output_cost_per_token = case_when(model == "ministral-8b-latest" ~ 0.1/2000000,
+                                            TRUE ~ output_cost_per_token)) |>
+    summarise(
+      api_type = unique(api_type),
+      input_cost = sum(input_tokens*input_cost_per_token, na.rm = TRUE),
+      output_cost = sum(input_tokens*output_cost_per_token, na.rm = TRUE),
+      total_cost = input_cost+output_cost,
+      input_cost_per_token = unique(input_cost_per_token),
+      output_cost_per_token = unique(output_cost_per_token),
+      input_tokens = sum(input_tokens, na.rm = TRUE),
+      output_tokens = sum(output_tokens, na.rm = TRUE),
+      total_tokens = input_tokens + output_tokens)
+  res
 }
--- _targets.R
+++ _targets.R
@@ -12,38 +12,7 @@ library(tidyverse)
 tar_option_set(
   packages = c("R.AI", "tidyverse"), # Packages that your targets need for their tasks.
   imports = c("R.AI"),
-  # format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  # controller = crew::crew_controller_local(workers = 6, seconds_idle = 60)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  #
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
+  controller = crew::crew_controller_local(workers = 20, seconds_idle = 60)
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -63,8 +32,42 @@ list(
       select(country_name:year, starts_with("v2x_polyarchy")) |>
       mutate(country = country_name) |>
       as_tibble() |>
+      filter(!is.na(v2x_polyarchy))
+  ),
+  tar_target(
+    name = vdem_post_1945,
+    command = vdemdata::vdem |>
+      select(country_name:year, starts_with("v2x_polyarchy")) |>
+      mutate(country = country_name) |>
+      as_tibble() |>
+      filter(!is.na(v2x_polyarchy), year > 1945)
+  ),
+  tar_target(
+    name = vdem_sample,
+    command = vdemdata::vdem |>
+      select(country_name:year, starts_with("v2x_polyarchy")) |>
+      mutate(country = country_name) |>
+      as_tibble() |>
       filter(!is.na(v2x_polyarchy)) |>
-      slice_sample(n = 2000)
+      slice_sample(n = 1000)
+  ),
+  tar_group_count(
+    name = vdem_grouped,
+    command = vdemdata::vdem |>
+      select(country_name:year, starts_with("v2x_polyarchy")) |>
+      mutate(country = country_name) |>
+      as_tibble() |>
+      filter(!is.na(v2x_polyarchy)),
+    count = 3
+  ),
+  tar_group_by(
+    name = vdem_grouped_country,
+    command = vdemdata::vdem |>
+      select(country_name:year, starts_with("v2x_polyarchy")) |>
+      mutate(country = country_name) |>
+      as_tibble() |>
+      filter(!is.na(v2x_polyarchy)),
+    country_id
   ),
   tar_target(
     name = prompt_files,
@@ -72,7 +75,8 @@ list(
     format = "file"
   ),
   tar_eval(
-    values = prompt_files_df,
+    values = prompt_files_df |>
+      filter(!str_detect(dataset_name, "vdem_grouped")),
     tar_target(
       name = prompt,
       command = build_prompts_from_files(
@@ -83,77 +87,152 @@ list(
     )
   ),
   tar_eval(
-    values = responses_df,
+    values = prompt_files_df |>
+      filter(str_detect(dataset_name, "vdem_grouped")),
+    tar_target(
+      name = prompt,
+      command = build_prompts_from_files(
+        files = here::here("prompts", prompt_filename),
+        roles = "user",
+        api = api,
+        data = dataset),
+      pattern = map(dataset)
+    )
+  ),
+  tar_eval(
+    values = responses_df |>
+      filter(type == "single",
+             api == "gemini",
+             dataset_name == "vdem_grouped_country"),
     tar_target(
       name = responses,
-      command = call_api(
+      command = fun(
         prompts = prompt,
         model = model,
         prompt_name = prompt_name,
         rate_limit = 2,
         temperature = 0,
         max_tokens = 2000,
-        json_mode = FALSE)  |>
-        select(where(\(x) !all(is.na(x)))),
-      pattern = map(prompt)
+        json_mode = FALSE),
+      pattern = map(prompt),
+      error = "null"
     )
   ),
   tar_eval(
-    values = responses_df,
+    values = responses_df |>
+      filter(type == "batch",
+             dataset == "vdem",
+             api %in% c("openai", "mistral")),
+    tar_target(
+      name = responses,
+      command = fun(
+        prompts = prompt,
+        model = model,
+        max_tokens = 2000),
+      deployment = "main"
+    )
+  ),
+  tar_eval(
+    values = responses_df |>
+      filter(type == "batch",
+             dataset_name == "vdem",
+             api %in% c("openai", "mistral")),
     tar_target(
       name = responses_joined,
-      command = responses |>
-        bind_cols(dataset) |>
-        mutate(score = str_extract(
-          response,
-          "(?<=\\\\?<democracy\\\\?>)([0-9]*\\.?[0-9]+)(?=\\\\?<(/)?democracy\\\\?>)") |>
-            as.numeric()) |>
-        select(-starts_with("max_"), -litellm_provider,
-               -mode, -starts_with("supports_"),
-               -starts_with("tool"), -input_cost,
-               -output_cost, -total_cost,
-               -contains("_details"))
+      command = extract_and_join(responses, dataset),
+      deployment = "main",
+      error = "continue"
+    )
+  ),
+  tar_eval(
+    values = responses_df |>
+      filter(str_detect(dataset_name, "vdem_grouped"),
+             type == "batch"),
+    tar_target(
+      name = responses_joined,
+      command = extract_and_join(responses, dataset),
+      deployment = "main",
+      error = "continue",
+      pattern = map(responses, dataset)
+    )
+  ),
+  tar_eval(
+    values = responses_df |>
+      filter(str_detect(dataset_name, "vdem_grouped"),
+             type == "single"),
+    tar_target(
+      name = responses_joined,
+      command = extract_and_join_single(responses, dataset),
+      pattern = map(responses, dataset)
+    )
+  ),
+  tar_eval(
+    values = responses_df |>
+      filter(type == "batch",
+             dataset_name == "vdem_grouped"),
+    tar_target(
+      name = responses,
+      command = fun(
+        prompts = prompt,
+        model = model,
+        max_tokens = 2000),
+      deployment = "main",
+      pattern = map(prompt, dataset),
+      iteration = "list"
     )
   ),
   tar_target(
     name = combined_responses,
-    command = dplyr::bind_rows(!!!responses_df$responses_joined),
+    command = dplyr::bind_rows(!!!responses_df$responses_joined[
+      responses_df$api %in% c("openai", "mistral", "claude", "gemini") &
+        responses_df$dataset_name != "fh"]) |>
+      filter(!is.na(model)) |>
+      left_join(responses_df |>
+                  ungroup() |>
+                  select(model, api, type, dataset_name) |>
+                  rename(api_name = api,
+                         api_type = type) |>
+                  filter(dataset_name != "fh") |>
+                  mutate(model = case_when(model == "gpt-4o" ~ "gpt-4o-2024-08-06",
+                                           model == "gpt-4o-mini" ~ "gpt-4o-mini-2024-07-18",
+                                           TRUE ~ model))),
     deployment = "main"
   ),
   tar_target(
     name = combined_responses_wide,
     command = combined_responses  |>
-      pivot_wider(id_cols = c("prompt_id", "country", "year", "country_id",
+      select(all_of(c("country", "year", "country_id",
+                      "model", "v2x_polyarchy",
+                      "v2x_polyarchy_codelow",
+                      "v2x_polyarchy_codehigh", "v2x_polyarchy_sd",
+                      "response", "score"))) |>
+      pivot_wider(id_cols = c("country", "year", "country_id",
                               "v2x_polyarchy", "v2x_polyarchy_codelow",
                               "v2x_polyarchy_codehigh", "v2x_polyarchy_sd"),
                   names_from = "model",
                   values_from = c(score, response)) |>
-      janitor::clean_names()
+      janitor::clean_names(),
+    deployment = "main"
   ),
   tar_target(
     name = correlations,
     command = combined_responses_wide |>
       select(v2x_polyarchy, starts_with("score")) |>
       as.matrix() |>
-      Hmisc::rcorr()
+      Hmisc::rcorr(),
+    deployment = "main"
   ),
   tar_target(
     name = combined_responses_summary,
     command = combined_responses |>
-      group_by(country_id, country_text_id, country, year, v2x_polyarchy) |>
+      group_by(country_id, country_text_id, country, year, v2x_polyarchy,
+               v2x_polyarchy_codelow, v2x_polyarchy_codehigh, v2x_polyarchy_sd) |>
       summarise(n = sum(!is.na(score)),
                 score = list(Hmisc::smean.cl.boot(score) |>
                                as_tibble_row())) |>
       unnest(score) |>
-      ungroup()
-  ),
-  tar_target(
-    name = estimated_costs,
-    command = combined_responses |>
-      group_by(api) |>
-      summarise(est_input_cost = sum(input_tokens*input_cost_per_token, na.rm = TRUE),
-                est_output_cost = sum(output_tokens*output_cost_per_token, na.rm = TRUE),
-                est_total_cost = est_input_cost + est_output_cost)
+      ungroup(),
+    deployment = "main"
   ),
   tar_target(
     name = rmse_per_year_summary,
@@ -161,6 +240,20 @@ list(
       group_by(year) |>
       summarise(rmse = sqrt(sum((v2x_polyarchy-Mean)^2)),
                 n = n())
+  ),
+  tar_url(
+    name = model_costs_url,
+    command = "https://raw.githubusercontent.com/BerriAI/litellm/refs/heads/main/model_prices_and_context_window.json"
+  ),
+  tar_target(
+    name = model_costs,
+    command = load_model_costs(model_costs_url),
+    deployment = "main"
+  ),
+  tar_target(
+    name = estimated_costs,
+    command = estimate_costs(combined_responses, model_costs),
+    deployment = "main"
   ),
   tar_target(
     name = rmse_per_country_summary,
